14d66cd340d2c774263a995d555117cc647efd0a
ACBC <camiloblandon69@gmail.com>
2023-08-19 17:36:35 -0500

    Tipo: Nuevas Características Alcance: Añadir más módulos (proyects, skills, footer) Asunto: Añadir los componentes de proyects, skills y footer Descripción: Se han agregado los módulos de proyects, skills y footer al sitio web. Se implementaron componentes específicos para cada módulo, incluyendo una sección de proyectos, habilidades destacadas y un pie de página informativo. Los componentes fueron diseñados con tecnologías como React y CSS para mejorar la experiencia del usuario y proporcionar información relevante.
    Este resumen captura los aspectos esenciales del commit y su impacto en el desarrollo del sitio web.

---
+++ Portfolio/src/data/dataProyects.ts
@@ -0,0 +1,21 @@
+interface ProyectType {
+  link: string;
+  img: string;
+  title: string;
+  hash: string[];
+}
+
+export const proyects: ProyectType[] = [
+  {
+    link: "",
+    img: "",
+    title: "Gif Finder",
+    hash: ["Nextjs", "React", "Tailwindcss"],
+  },
+  {
+    link: "",
+    img: "",
+    title: "To Do",
+    hash: ["Nextjs", "React", "Tailwindcss"],
+  },
+];

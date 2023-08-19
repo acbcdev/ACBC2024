6dc2603a841a6412b38a4a22ffea9de0f6fe8b2b
ACBC <camiloblandon69@gmail.com>
2023-08-19 17:34:58 -0500

    feat(añadir mas modulo): añadir los componentes mencionados como proyects skills footer
    En este commit, se ha agregado el módulo de 'proyects', 'skills' y 'footer'. Se han añadido los componentes correspondientes para cada uno de estos módulos. El módulo 'proyects' incluye la sección de proyectos, el módulo 'skills' contiene las habilidades destacadas y el módulo 'footer' se ha incorporado al final del sitio web.
    
    Detalles adicionales:
    - En el módulo 'proyects', se ha creado una sección para mostrar los proyectos recientes.
    - El módulo 'skills' ahora muestra las habilidades clave del usuario.
    - El 'footer' ha sido diseñado e implementado con enlaces útiles y la información de contacto.
    
    [Descripción detallada]
    Se ha realizado la implementación de los componentes mencionados siguiendo las mejores prácticas de diseño y desarrollo web. Se han utilizado tecnologías como React y CSS para lograr una integración fluida con el sitio web existente. Este commit contribuye a mejorar la experiencia general del usuario al proporcionar información relevante en los módulos de 'proyects', 'skills' y 'footer'.

--- Portfolio/src/components/Header.tsx
+++ Portfolio/src/components/Header.tsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 type dataH = { text: string; id: string };
 const dataHeader: dataH[] = [
   { text: "About me", id: "about" },
-  { text: "Proyects", id: "proyecy" },
-  { text: "Habilidades", id: "habilidades" },
+  { text: "Skills", id: "skills" },
+  { text: "Proyects", id: "projects" },
   { text: "Contact me", id: "contact" },
 ];
 export const Header = () => {
@@ -60,7 +60,7 @@ export const Header = () => {
           <div
             className={`${
               isMobileMenuOpen ? "block" : "hidden"
-            } sm:hidden border-4 border-white rounded-3xl bg-black`}
+            } sm:hidden border-4 border-white rounded-3xl bg-transparent`}
             id="mobile-menu"
           >
             <div className="px-2 pb-3 mt-60 pt-2">
@@ -68,7 +68,7 @@ export const Header = () => {
                 <a
                   key={item.id}
                   href={`#${item.id}`}
-                  className="text-white border-4 rounded-3xl border-black  hover:border-white block  px-3 py-2 text-base font-medium"
+                  className="text-white border-4 rounded-3xl border-transparent  hover:border-white block  px-3 py-2 text-base font-medium"
                 >
                   {item.text}
                 </a>
@@ -82,7 +82,7 @@ export const Header = () => {
                 <a
                   key={item.id}
                   href={`#${item.id}`}
-                  className=" text-white border-4 rounded-3xl border-black  hover:border-white px-3 py-2 text-sm font-medium"
+                  className=" text-white border-4 rounded-3xl border-transparent  hover:border-white px-3 py-2 text-sm font-medium"
                 >
                   {item.text}
                 </a>
@@ -92,105 +92,5 @@ export const Header = () => {
         </div>
       </div>
     </nav>
-    // <nav className="bg-blue-500 p-4">
-    //   <div className="mx-auto flex justify-between items-center">
-    //     <div className="text-white font-semibold text-lg">My Portfolio</div>
-    //     <div className="lg:hidden grid place-content-center">
-    //       <button onClick={toggleMobileMenu} className="text-white">
-    //         <svg
-    //           className="h-6 w-6"
-    //           fill="none"
-    //           stroke="currentColor"
-    //           viewBox="0 0 24 24"
-    //           xmlns="http://www.w3.org/2000/svg"
-    //         >
-    //           <path
-    //             strokeLinecap="round"
-    //             strokeLinejoin="round"
-    //             strokeWidth="2"
-    //             d="M4 6h16M4 12h16M4 18h16"
-    //           />
-    //         </svg>
-    //       </button>
-    //     </div>
-    //   </div>
-    //   <div className={`${isMobileMenuOpen ? "block" : "hidden"} lg:hidden`}>
-    //     <ul className="mt-4 space-y-2">
-    //       {dataHeader.map((item) => (
-    //         <li className="bg-emerald-600">
-    //           <a href={item.id} className=" text-white hover:underline">
-    //             {item.text}
-    //           </a>
-    //         </li>
-    //       ))}
-    //     </ul>
-    //   </div>
-    // </nav>
-
-    // <nav className=" text-white flex mt-4">
-    //   <div className="container mx-auto flex justify-between items-center">
-    //     <a href="/" className="flex items-center">
-    //       <img
-    //         src="../../public/favicon.svg"
-    //         alt="Astro"
-    //         className="h-10 mr-2"
-    //       />
-    //       <span className="font-bold">Astro</span>
-    //     </a>
-    //     <button
-    //       className="text-white p-2 rounded-full"
-    //       onClick={toggleMobileMenu}
-    //     >
-    //       <svg
-    //         className="h-6 w-6"
-    //         fill="none"
-    //         stroke="currentColor"
-    //         viewBox="0 0 24 24"
-    //         xmlns="http://www.w3.org/2000/svg"
-    //       >
-    //         <path
-    //           strokeLinecap="round"
-    //           strokeLinejoin="round"
-    //           strokeWidth="2"
-    //           d="M4 6h16M4 12h16M4 18h16"
-    //         />
-    //       </svg>
-    //     </button>
-    //   </div>
-    //   <div className="hidden sm:flex flex-col pt-5">
-    //     {isMobileMenuOpen && (
-    //       <ul className="bg-stone-400">
-    //         <li>
-    //           <a href="/about">About</a>
-    //         </li>
-    //         <li>
-    //           <a href="/docs">Docs</a>
-    //         </li>
-    //         <li>
-    //           <a href="/blog">Blog</a>
-    //         </li>
-    //         <li>
-    //           <a href="/contact">Contact</a>
-    //         </li>
-    //       </ul>
-    //     )}
-    //     {!isMobileMenuOpen && (
-    //       <ul className="list-unstyled">
-    //         <li>
-    //           <a href="/about">About</a>
-    //         </li>
-    //         <li>
-    //           <a href="/docs">Docs</a>
-    //         </li>
-    //         <li>
-    //           <a href="/blog">Blog</a>
-    //         </li>
-    //         <li>
-    //           <a href="/contact">Contact</a>
-    //         </li>
-    //       </ul>
-    //     )}
-    //   </div>
-    // </nav>
   );
 };

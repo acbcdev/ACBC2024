6e6213012fcded8227a40f635adc2d23d3ffd1ce
acbc <acbc.dev@gmail.com>
2024-04-06 22:06:54 -0500

    Update getProjects function to handle pages without properties

--- Portfolio/src/utils/notion.ts
+++ Portfolio/src/utils/notion.ts
@@ -5,28 +5,36 @@ const notion = new Client({
   auth: import.meta.env.NOTION_TOKEN,
 });
 
-interface ProyectType {
+export interface ProyectType {
   link: string;
   github: string;
   img: string;
   name: string;
   tags: string[];
+  descripcion: string;
 }
 
 export const getProjects = async () => {
   const query = { database_id: import.meta.env.NOTION_DATABASE_ID };
   const { results } = await notion.databases.query(query);
-  return results.map((page) => {
-    const { properties } = page;
-    const { github, link, img, tags, name, descripcion } = properties;
+  return results
+    .map((page) => {
+      if ("properties" in page) {
+        const { properties } = page;
+        const { github, link, img, tags, name, descripcion } = properties;
 
-    return {
-      descripcion: descripcion.rich_text[0].plain_text,
-      github: github.url,
-      link: link?.url,
-      img: img?.files[0]?.file.url,
-      tags: tags.multi_select.map((tag) => tag.name),
-      name: name.title[0].plain_text,
-    };
-  });
+        return {
+          descripcion: descripcion.rich_text[0].plain_text,
+          github: github.url,
+          link: link?.url,
+          img: img?.files[0]?.file.url,
+          tags: tags.multi_select.map((tag) => tag.name),
+          name: name.title[0].plain_text,
+        };
+      } else {
+        // return a default object or null when properties does not exist on page
+        return null;
+      }
+    })
+    .filter(Boolean); // filter out null values
 };

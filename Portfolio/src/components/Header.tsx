94945e60a3464317391cc4b96a1362b1b9102581
acbc <acbc.dev@gmail.com>
2024-05-08 18:54:05 -0500

    chore: Add Learning component to display current learning

--- Portfolio/src/components/Header.tsx
+++ Portfolio/src/components/Header.tsx
@@ -1,94 +1,92 @@
 import { useState } from "react";
 type dataH = { text: string; id: string };
 const dataHeader: dataH[] = [
-  { text: "About me", id: "about" },
-  { text: "Skills", id: "skills" },
-  { text: "Proyects", id: "projects" },
+	{ text: "About me", id: "about" },
+	{ text: "Skills", id: "skills" },
+	{ text: "Projects", id: "projects" },
+	{ text: "Current Learning", id: "learning" },
 ];
 export const Header = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+	const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-  return (
-    <nav>
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="flex absolute inset-y-0 left-0 sm:hidden items-center ">
-            <button
-              type="button"
-              className="relative inline-flex items-center justify-center  p-2 border-4 border-transparent rounded-3xl hover:border-white text-white  "
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-              onClick={toggleMobileMenu}
-            >
-              {isMobileMenuOpen ? (
-                <div>
-                  <svg
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                </div>
-              ) : (
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                </svg>
-              )}
-            </button>
-          </div>
-          <div
-            className={`${
-              isMobileMenuOpen ? "block" : "hidden"
-            } sm:hidden border-4 border-white rounded-3xl bg-transparent`}
-            id="mobile-menu"
-          >
-            <div className="px-2 pb-3 mt-60 pt-2">
-              {dataHeader.map((item) => (
-                <a
-                  key={item.id}
-                  href={`#${item.id}`}
-                  className="text-white  rounded-3xl outline-4 outline-transparent  hover:outline-white block  px-3 py-2 text-base font-medium"
-                >
-                  {item.text}
-                </a>
-              ))}
-            </div>
-          </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="hidden md:block mx-auto space-x-4">
-              {dataHeader.map((item) => (
-                <a
-                  key={item.id}
-                  href={`#${item.id}`}
-                  className=" text-white outline-4 rounded-3xl  duration-300 hover:text-xl hover:outline-4 hover:outline-white px-3 py-2 text-lg font-medium"
-                >
-                  {item.text}
-                </a>
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </nav>
-  );
+	const toggleMobileMenu = () => {
+		setIsMobileMenuOpen(!isMobileMenuOpen);
+	};
+	return (
+		<nav>
+			<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+				<div className="relative flex h-16 items-center justify-between">
+					<div className="flex absolute inset-y-0 left-0 sm:hidden items-center ">
+						<button
+							type="button"
+							className="relative inline-flex items-center justify-center  p-2 border-4 border-transparent rounded-3xl hover:border-white text-white  "
+							aria-controls="mobile-menu"
+							aria-expanded="false"
+							onClick={toggleMobileMenu}
+						>
+							{isMobileMenuOpen ? (
+								<div>
+									<SvgMenu />
+								</div>
+							) : (
+								<SvgMenu />
+							)}
+						</button>
+					</div>
+					<div
+						className={`${
+							isMobileMenuOpen ? "block" : "hidden"
+						} sm:hidden border-4 border-white rounded-3xl bg-transparent`}
+						id="mobile-menu"
+					>
+						<div className="px-2 pb-3 mt-60 pt-2">
+							{dataHeader.map((item) => (
+								<a
+									key={item.id}
+									href={`#${item.id}`}
+									className="text-white  rounded-3xl outline-4 outline-transparent  hover:outline-white block  px-3 py-2 text-base font-medium"
+								>
+									{item.text}
+								</a>
+							))}
+						</div>
+					</div>
+					<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+						<div className="hidden md:block mx-auto space-x-4">
+							{dataHeader.map((item) => (
+								<a key={item.id} href={`#${item.id}`}>
+									<button
+										type="button"
+										className="text-2xl px-2 py-1 rounded-3xl transition duration-300 hover:scale-110 hover:outline hover:outline-4 "
+									>
+										{item.text}
+									</button>
+								</a>
+							))}
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
 };
+
+function SvgMenu() {
+	return (
+		<svg
+			className="h-6 w-6"
+			fill="none"
+			viewBox="0 0 24 24"
+			stroke="currentColor"
+		>
+			<title>Menu Icon</title> // Add a title element with a non-empty
+			alternative text
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				strokeWidth="2"
+				d="M4 6h16M4 12h16M4 18h16"
+			/>
+		</svg>
+	);
+}

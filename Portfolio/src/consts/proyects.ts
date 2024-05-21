85ad828fd14bf4d0650d21723396296b44bd7559
acbc <acbc.dev@gmail.com>
2024-05-21 11:01:21 -0500

    Refactor Proyects.astro to improve consistency and readability

--- Portfolio/src/consts/proyects.ts
+++ Portfolio/src/consts/proyects.ts
@@ -1,14 +1,7 @@
 import type { ProyectType } from "@/types/proyects";
 
 export const proyects: ProyectType[] = [
-  {
-    descripcion: 'un Buscador de Gifs con el cual ',
-    github: 'https://github.com/ACBC-DEV/Gifty',
-    link: '',
-    img: 'https://prod-files-secure.s3.us-west-2.amazonaws.com/e40427eb-cfa4-4177-806e-9784d4388bbd/9f8e0709-93a7-4251-872e-82d1aadfb90b/Captura_desde_2024-05-16_14-37-12.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45HZZMZUHI%2F20240521%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20240521T140351Z&X-Amz-Expires=3600&X-Amz-Signature=0573b316d76a973b540ad6523050a2c2a51eeb1b65e07ce542c52a7fe5eb58a1&X-Amz-SignedHeaders=host&x-id=GetObject',
-    tags: ['Nextjs', 'Tailwindcss', 'React'],
-    name: 'Gifty'
-  },
+
   {
     descripcion: 'Un conjunto de Calculadores para Aprender Programacion',
     github: 'https://github.com/ACBC-DEV/SmartMoney',
@@ -20,9 +13,17 @@ export const proyects: ProyectType[] = [
   {
     descripcion: 'un generador de shortLinks',
     github: 'https://github.com/ACBC-DEV/LinkMinify',
-    link: 'https://github.com/ACBC-DEV/LinkMinify',
+    link: 'https://link-minify.vercel.app/',
     img: 'https://prod-files-secure.s3.us-west-2.amazonaws.com/e40427eb-cfa4-4177-806e-9784d4388bbd/44b4c09f-9bd7-41eb-8016-980b9c519e64/Captura_desde_2024-03-08_16-53-08.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45HZZMZUHI%2F20240521%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20240521T140351Z&X-Amz-Expires=3600&X-Amz-Signature=da9abf5e4bf7d55d1fb9af34ad8aa10b3e62c2eadb39e1727855e9279d46ff4e&X-Amz-SignedHeaders=host&x-id=GetObject',
     tags: ['Nextjs', 'Tailwindcss', 'mongodb', 'React'],
     name: 'LinkMinify'
-  }
+  },
+  {
+    descripcion: 'un Buscador de Gifs con el cual ',
+    github: 'https://github.com/ACBC-DEV/Gifty',
+    link: 'https://github.com/ACBC-DEV/Gifty',
+    img: 'https://prod-files-secure.s3.us-west-2.amazonaws.com/e40427eb-cfa4-4177-806e-9784d4388bbd/9f8e0709-93a7-4251-872e-82d1aadfb90b/Captura_desde_2024-05-16_14-37-12.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45HZZMZUHI%2F20240521%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20240521T140351Z&X-Amz-Expires=3600&X-Amz-Signature=0573b316d76a973b540ad6523050a2c2a51eeb1b65e07ce542c52a7fe5eb58a1&X-Amz-SignedHeaders=host&x-id=GetObject',
+    tags: ['Nextjs', 'Tailwindcss', 'React'],
+    name: 'Gifty'
+  },
 ]

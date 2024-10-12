8114d3f1f84a7a988163ae20fbdfbda71980320e
acbc <acbc.dev@gmail.com>
2024-10-12 17:56:52 -0500

    (CHANGE) the images proyects

--- Portfolio/src/consts/proyects.ts
+++ Portfolio/src/consts/proyects.ts
@@ -1,36 +1,28 @@
 import type { ProyectType } from "@/types/proyects";
 
 export const projects: ProyectType[] = [
-  {
-    description: 'A set of calculators ',
-    github: 'https://github.com/ACBC-DEV/SmartMoney',
-    link: 'https://smart-money-five.vercel.app/',
-    img: '/img/smartmoney.avif',
-    tags: ['Astro', 'SolidJS', 'Tailwindcss'],
-    name: 'SmartMoney'
-  },
-  {
-    description: 'A short link generator',
-    github: 'https://github.com/ACBC-DEV/LinkMinify',
-    link: 'https://link-minify.vercel.app/',
-    img: '/img/link-minify.avif',
-    tags: ['Nextjs', 'Tailwindcss', 'Mongodb',],
-    name: 'LinkMinify'
-  },
-  // {
-  //   description: 'A search of gifs engine',
-  //   github: 'https://github.com/ACBC-DEV/Gifty',
-  //   link: 'https://github.com/ACBC-DEV/Gifty',
-  //   img: '/img/gifty.avif',
-  //   tags: ['Nextjs', 'Tailwindcss', 'React'],
-  //   name: 'Gifty'
-  // },
-  {
-    name: 'Trello Clone',
-    github: 'https://github.com/ACBC-DEV/trello-clone',
-    link: 'https://trello-clone-tau-eight.vercel.app/',
-    img: '/img/trello.avif',
-    tags: ['Angular', 'Tailwindcss',],
-    description: 'Trello Clone with Angular '
-  }
-]
+	{
+		description: "A short link generator",
+		github: "https://github.com/ACBC-DEV/LinkMinify",
+		link: "https://link-minify.vercel.app/",
+		img: "/img/link.webp",
+		tags: ["Nextjs", "Tailwindcss", "Mongodb"],
+		name: "LinkMinify",
+	},
+	{
+		name: "Trello Clone",
+		github: "https://github.com/ACBC-DEV/trello-clone",
+		link: "https://trello-clone-tau-eight.vercel.app/",
+		img: "/img/trello.webp",
+		tags: ["Angular", "Tailwindcss"],
+		description: "Trello Clone with Angular ",
+	},
+	{
+		description: "A set of calculators ",
+		github: "https://github.com/ACBC-DEV/SmartMoney",
+		link: "https://smart-money-five.vercel.app/",
+		img: "/img/smart.webp",
+		tags: ["Astro", "SolidJS", "Tailwindcss"],
+		name: "SmartMoney",
+	},
+];

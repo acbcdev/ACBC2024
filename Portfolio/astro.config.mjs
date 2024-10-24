e184b73e602fa60f4402c72d067e06dc5b9bb892
ACBC <acbc.dev@gmail.com>
2024-10-24 10:38:25 -0500

    (add) hybrid output

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://acbc.dev:",
-	integrations: [tailwind({ nesting: true })],
+	integrations: [
+		tailwind({
+			nesting: true,
+		}),
+	],
+	output: "hybrid",
+	adapter: vercel(),
 });

96fb46dffcf633f85c981178bc59d17735bc8d78
ACBC <acbc.dev@gmail.com>
2024-10-24 11:04:24 -0500

    (change) the previus output the static

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,16 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
 // https://astro.build/config
 export default defineConfig({
 	site: "https://acbc.dev:",
+	output: "static",
 	integrations: [
 		tailwind({
 			nesting: true,
 		}),
 	],
-	output: "hybrid",
-	adapter: vercel(),
 });

99826ce7c22f0082af83654842a69de30ba44f29
ACBC <acbc.dev@gmail.com>
2024-10-24 13:00:15 -0500

    (change) lowercase to CapitalCase

--- Portfolio/src/consts/skills.ts
+++ Portfolio/src/consts/skills.ts
@@ -3,16 +3,16 @@ interface skillType {
   name: string;
 }
 export const SkillsICons: skillType[] = [
-  { src: "/svg/file-type-html.svg", name: "html icon" },
-  { src: "/svg/css.svg", name: "css icon" },
-  { src: "/svg/javascript-js.svg", name: "javascript icon" },
-  { src: "/svg/tailwind-css.svg", name: "tailwindcss icon" },
-  { src: "/svg/react.svg", name: "react icon" },
-  { src: "/svg/nextjs.svg", name: "nextjs icon" },
-  { src: "/svg/typescript-icon.svg", name: "typescript icon" },
-  { src: "/svg/astro_dark.svg", name: "astro" },
-  { src: "/svg/solidjs.svg", name: "solid-js" },
-  { src: "/svg/bash_dark.svg", name: "bash" },
+  { src: "/svg/file-type-html.svg", name: "HTML" },
+  { src: "/svg/css.svg", name: "CSS" },
+  { src: "/svg/javascript-js.svg", name: "Javascript" },
+  { src: "/svg/tailwind-css.svg", name: "Tailwindcss" },
+  { src: "/svg/react.svg", name: "React" },
+  { src: "/svg/nextjs.svg", name: "Next.js" },
+  { src: "/svg/typescript-icon.svg", name: "Typescript" },
+  { src: "/svg/astro_dark.svg", name: "Astro.js" },
+  { src: "/svg/solidjs.svg", name: "Solid.js" },
+  { src: "/svg/bash_dark.svg", name: "Bash" },
   {
     name: "Angular",
     src: "/svg/angular.svg",

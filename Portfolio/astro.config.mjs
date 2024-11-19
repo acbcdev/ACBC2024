1a91a2be56801b8df74e157506b4b2a4fc4aaab6
ACBC <acbc.dev@gmail.com>
2024-11-18 20:43:44 -0500

    fix config

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
 	site: "https://acbc.dev:",
+	output: "server",
 	integrations: [
 		tailwind({
 			nesting: true,

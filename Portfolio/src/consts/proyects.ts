c7e4d73dd14b49489f3d8fef50264a7f83c3226f
ACBC <acbc.dev@gmail.com>
2024-11-18 20:27:34 -0500

    new proyects and new description

--- Portfolio/src/consts/proyects.ts
+++ Portfolio/src/consts/proyects.ts
@@ -2,27 +2,54 @@ import type { ProyectType } from "@/types/proyects";
 
 export const projects: ProyectType[] = [
 	{
-		description: "A short link generator",
-		github: "https://github.com/ACBC-DEV/LinkMinify",
-		link: "https://link.acbc.dev/",
-		img: "/img/link.webp",
-		tags: ["Nextjs", "Tailwindcss", "Mongodb"],
-		name: "LinkMinify",
+		link: "https://runts.acbc.dev/",
+		github: "https://github.com/ACBC-DEV/RunTS",
+		img: "/img/runts.webp",
+		tags: ["React", "Playground", "WebWorker"],
+		name: "RunTS",
+		iconTecnology: "/svg/react.svg",
+		description:
+			"🎯 A TypeScript playground for running typescript code with: 🎨 Themes, 🔄 Auto-refresh, 🚀 Live execution, 💝 Open Source,🤝 Open to contributions",
 	},
+	{
+		name: "Spooky Histories",
+		link: "https://hackthon-cloudinary.acbc.dev/",
+		github: "https://github.com/ACBC-DEV/hackthon-cloudinary",
+		img: "/img/spooky.webp",
+		tags: ["Nextjs", "Tailwindcss", "Cloudinary"],
+		description:
+			"Interactive AI storyteller that generates dynamic spooky tales from your images. Choose your path, modify images, and craft your own haunting adventure 🎬👻",
+		iconTecnology: "/svg/nextjs.svg",
+	},
+
 	{
 		name: "Trello Clone",
 		github: "https://github.com/ACBC-DEV/trello-clone",
 		link: "https://trello.acbc.dev/",
 		img: "/img/trello.webp",
 		tags: ["Angular", "Tailwindcss"],
-		description: "Trello Clone with Angular ",
+		description:
+			"🔗 Trello Clone with Angular: 🚀 A Feature-Rich Task Management App 📝 with Drag-and-Drop Functionality 🤏 and Local Persistence 💾",
+		iconTecnology: "/svg/angular.svg",
 	},
 	{
-		description: "A set of calculators ",
+		description:
+			"🔗 Short Link URL Generator: 🚀 Create Compact and Shareable Links Effortlessly ✂️",
+		github: "https://github.com/ACBC-DEV/LinkMinify",
+		link: "https://link.acbc.dev/",
+		img: "/img/link.webp",
+		tags: ["Nextjs", "Tailwindcss", "Mongodb"],
+		name: "LinkMinify",
+		iconTecnology: "/svg/nextjs.svg",
+	},
+	{
+		description:
+			"📊 A Set of Financial Calculators: 🧮 Loan Payments, Investment Growth 📈, Savings Tracking 💰, Budget Planning 📝, ROI Calculation 🚀, and Tax Management 💵 ",
 		github: "https://github.com/ACBC-DEV/SmartMoney",
 		link: "https://smart-money-five.vercel.app/",
 		img: "/img/smart.webp",
 		tags: ["Astro", "SolidJS", "Tailwindcss"],
 		name: "SmartMoney",
+		iconTecnology: "/svg/astro_dark.svg",
 	},
 ];

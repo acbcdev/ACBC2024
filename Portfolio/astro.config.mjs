9abcdd835ab1266cd9fcee208723083e863853f4
ACBC <acbc.dev@gmail.com>
2024-11-19 15:12:39 -0500

    fix bug ui slot

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 export default defineConfig({
 	site: "https://acbc.dev:",
-	output: "hybrid",
+	output: "static",
 	integrations: [
 		tailwind({
 			nesting: true,

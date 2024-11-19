1cd0cb3aa540f9d15757e702485de3ebf6863ebb
ACBC <acbc.dev@gmail.com>
2024-11-18 19:45:33 -0500

    group seactions

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -1,9 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 export default defineConfig({
 	site: "https://acbc.dev:",
-	output: "server",
 	integrations: [
 		tailwind({
 			nesting: true,

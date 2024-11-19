8dc7bec30b4e2829eee4a1401a09bb5b93393a79
ACBC <acbc.dev@gmail.com>
2024-11-19 15:07:14 -0500

    change ouput way

--- Portfolio/astro.config.mjs
+++ Portfolio/astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
 	site: "https://acbc.dev:",
-	output: "server",
+	output: "hybrid",
 	integrations: [
 		tailwind({
 			nesting: true,
